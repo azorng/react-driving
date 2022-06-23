620e34b0cfc134247895af54d9280a9b43630796
Azor <i@azor.net>
2022-06-23 18:55:21 +0200

    add lib

---
+++ src/ui/lib.ts
@@ -0,0 +1,10 @@
+import { Position } from "core/types";
+
+export default {
+  posToStyle(position: Position) {
+    return {
+      left: `calc(${position[0]}px + 50vw)`,
+      top: `calc(${position[1] - position[1] * 2}px + 50vh`,
+    };
+  },
+};
